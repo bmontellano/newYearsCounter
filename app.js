dc4ebc334b366bf2c59f9ebad52acdf1adaf82db
Brian Elias Montellano <bmontellano@gmail.com>
2020-12-28 12:52:59 -0800

    New Years countdown calculation completed!

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 var spanDays = document.getElementById('days');
+var spanSeconds = document.getElementById('seconds');
 var firstElem = document.getElementById('first');
+var daysLeft = Math.floor((new Date(2021, 0, 1).getTime() - Date.now()) / 86400000);
+var secondsLeft = (((new Date(2021, 0, 1).getTime() - Date.now()) / 86400000) - daysLeft) * 86400;
+spanDays.innerText = daysLeft.toString();
+spanSeconds.innerText = secondsLeft.toFixed(0)
+
 console.log("This should be Span for days ---> ", spanDays);
 console.log("This should be first h1 ---> ", firstElem);
-setTimeout(() => {
-    spanDays.innerText = "3"
-}, 2000);
+
+var timeInterval = setInterval(() => {
+    daysLeft = Math.floor((new Date(2021, 0, 1).getTime() - Date.now()) / 86400000);
+    secondsLeft = (((new Date(2021, 0, 1).getTime() - Date.now()) / 86400000) - daysLeft) * 86400;
+    if (daysLeft < 0) {
+        clearInterval(timeInterval);
+    }
+    spanDays.innerText = daysLeft.toString();
+    spanSeconds.innerText = secondsLeft.toFixed(0);
+    console.log("interval running")
+}, 1000);
+
+
+console.log(daysLeft);
