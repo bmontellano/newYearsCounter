9816db8a2764f10c58c3d76b0c6bd411ded3c630
Brian Elias Montellano <bmontellano@gmail.com>
2021-01-04 20:55:34 -0800

    some improvements, new year message, countdown is complete

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
+var checkTime = new Date(2021, 0, 1).getTime() - Date.now();
 var spanDays = document.getElementById('days');
 var spanSeconds = document.getElementById('seconds');
 var firstElem = document.getElementById('first');
 var daysLeft = Math.floor((new Date(2021, 0, 1).getTime() - Date.now()) / 86400000);
 var secondsLeft = (((new Date(2021, 0, 1).getTime() - Date.now()) / 86400000) - daysLeft) * 86400;
-spanDays.innerText = daysLeft.toString();
-spanSeconds.innerText = secondsLeft.toFixed(0)
 
-console.log("This should be Span for days ---> ", spanDays);
-console.log("This should be first h1 ---> ", firstElem);
+console.log(.6*60, "minutes");
 
-var timeInterval = setInterval(() => {
-    daysLeft = Math.floor((new Date(2021, 0, 1).getTime() - Date.now()) / 86400000);
-    secondsLeft = (((new Date(2021, 0, 1).getTime() - Date.now()) / 86400000) - daysLeft) * 86400;
-    if (daysLeft < 0) {
-        clearInterval(timeInterval);
-    }
+if(checkTime < 0) {
+    firstElem.innerHTML = "<p style='color:red;font-size:50px'>HAPPY NEW YEAR!! 2021</p>";
+} else {
     spanDays.innerText = daysLeft.toString();
     spanSeconds.innerText = secondsLeft.toFixed(0);
-    console.log("interval running")
-}, 1000);
+
+    console.log("This should be Span for days ---> ", spanDays);
+    console.log("This should be first h1 ---> ", firstElem);
+
+    var timeInterval = setInterval(() => {
+        checkTime = new Date(2021, 0, 1).getTime() - Date.now();
+        daysLeft = Math.floor((new Date(2021, 0, 1).getTime() - Date.now()) / 86400000);
+        secondsLeft = (((new Date(2021, 0, 1).getTime() - Date.now()) / 86400000) - daysLeft) * 86400;
+        if (checkTime < 0) {
+            firstElem.innerHTML = "<p style='color:red;font-size:50px'>HAPPY NEW YEAR!! 2021</p>";
+            clearInterval(timeInterval);
+        }
+        spanDays.innerText = daysLeft.toString();
+        spanSeconds.innerText = secondsLeft.toFixed(0);
+        console.log("interval running")
+    }, 1000);
+    console.log(daysLeft);
+}
 
 
-console.log(daysLeft);
