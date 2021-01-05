b793067835bf9fd55aaafa1a5f80ecf0fd8255e0
Brian Elias Montellano <bmontellano@gmail.com>
2021-01-04 21:47:21 -0800

    added gradient effect with style.opacity

--- app.js
+++ app.js
@@ -4,12 +4,28 @@ var spanSeconds = document.getElementById('seconds');
 var firstElem = document.getElementById('first');
 var daysLeft = Math.floor((new Date(2021, 0, 1).getTime() - Date.now()) / 86400000);
 var secondsLeft = (((new Date(2021, 0, 1).getTime() - Date.now()) / 86400000) - daysLeft) * 86400;
+var initVar = 0;
+var counter = 0;
 
 console.log(.6*60, "minutes");
 
 if(checkTime < 0) {
-    firstElem.innerHTML = "<p style='color:red;font-size:50px'>HAPPY NEW YEAR!! 2021</p>";
+    firstElem.style.opacity = "1.0";
+    firstElem.innerHTML = "<p style='color:red;font-size:50px; opacity:0.01'>HAPPY NEW YEAR!! 2021</p>";
+    var firstChild = firstElem.firstChild;
+    var initVar = parseFloat(firstElem.firstChild.style.opacity);
+    refInterval = setInterval(() => {
+        counter += 1;
+        if (counter > 10){
+            initVar += 0.01;
+            firstChild.style.opacity = initVar.toString();
+            if(parseFloat(firstChild.style.opacity) > 0.99 ) {
+                clearInterval(refInterval);  
+            }
+        }
+      },30); 
 } else {
+    firstElem.style.opacity = "1.0";
     spanDays.innerText = daysLeft.toString();
     spanSeconds.innerText = secondsLeft.toFixed(0);
 
