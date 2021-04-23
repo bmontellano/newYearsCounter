cb715b34cd505129f9f628ab49f86005e6a346bd
Brian Elias Montellano <bmontellano@gmail.com>
2021-04-22 18:11:24 -0700

    camera select alpha

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ const getCameraSelection = async () => {
   const videoDevices = devices.filter(device => device.kind === 'videoinput');
   const options = videoDevices.map(videoDevice => {
     sneakyCounter ++;
-    return `<option value="${videoDevice.deviceId}">${videoDevice.deviceId} ${sneakyCounter} </option>`;
+    return `<option value="${videoDevice.deviceId}">${videoDevice.label} ${sneakyCounter} </option>`;
   });
   cameraOptions.innerHTML = options.join('');
 };
